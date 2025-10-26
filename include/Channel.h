3ea6a4509e435713272000c769a5367560ff4246
xtopint00 <tadeas.topinka@gmail.com>
2025-10-27 00:35:33 +0100

    [FIX] moved private things below public identifier to make the private identifier actually do something

--- include/Channel.h
+++ include/Channel.h
@@ -10,17 +10,23 @@
 
 namespace Channel {
     class Channel {
-    private:
-        int sockfd_{-1};
-        int family_;
     public:
+
         Channel(int family, int protocol);
+
         ~Channel();
 
-        ssize_t sendPacket(const void* data, size_t len, const sockaddr* destination, socklen_t detinationLength);
-        ssize_t receivePacket(void* buffer, size_t len, sockaddr* source, socklen_t* sourceLength);
+        ssize_t sendPacket(const void* data, size_t len, const sockaddr* destination, socklen_t destinationLength) const;
+
+        ssize_t receivePacket(void* buffer, size_t len, sockaddr* source, socklen_t* sourceLength) const;
 
         [[nodiscard]] int family() const { return family_; }
+
+    private:
+
+        int sockfd_{-1};
+
+        int family_;
     };
 } // Channel
 
