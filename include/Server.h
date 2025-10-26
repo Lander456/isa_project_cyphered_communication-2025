c87e024626c45caf79d9e51605bbb93fb39c08ca
xtopint00 <tadeas.topinka@gmail.com>
2025-10-26 23:01:54 +0100

    Server class [NEW] created server class to contain all the server-side logic

---
+++ include/Server.h
@@ -0,0 +1,13 @@
+//
+// Created by tadeas on 2025-10-16.
+//
+
+#ifndef SERVER_H
+#define SERVER_H
+
+namespace Server {
+    class Server {
+    };
+} // Server
+
+#endif //SERVER_H
--- server/server.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by tadeas on 2025-10-16.
-//
-
-#include "server.h"
-
-namespace server {
-} // server
