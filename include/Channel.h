2c6c01fe9ec0521d63e115d953769e253991e8de
xtopint00 <tadeas.topinka@gmail.com>
2025-11-03 03:29:32 +0100

    [FEATURE] added more logic to creating sockets in order to discern between ICMP and ICMPV6 protocol based on address family, also added a timeout to the sockets and a polling system to ensure the socket waits for packets meant for the client (acks and such)

--- include/Channel.h
+++ include/Channel.h
@@ -7,26 +7,36 @@
 
 #include <unistd.h>
 #include <netdb.h>
+#include <chrono>
+#include <thread>
+
+#include "Packet.h"
 
 namespace Channel {
     class Channel {
     public:
 
-        Channel(int family, int protocol);
+        explicit Channel(int family);
 
         ~Channel();
 
-        ssize_t sendPacket(const void* data, size_t len, const sockaddr* destination, socklen_t destinationLength) const;
+        ssize_t sendPacket(const void* data, size_t len, const sockaddr* destination) const;
 
-        ssize_t receivePacket(void* buffer, size_t len, sockaddr* source, socklen_t* sourceLength) const;
+        ssize_t receivePacket(void* buffer, size_t len, sockaddr* source, socklen_t sourceLength) const;
+
+        Packet::IcmpPacket waitForResponse(uint16_t expectedId, sockaddr* source, socklen_t sourceLength);
 
         [[nodiscard]] int family() const { return family_; }
 
+        void setRemoteAddress(const sockaddr_storage& address) {remoteAddress_ = address;}
+        void setRemoteAddressLength(socklen_t remoteAddressLength) {remoteAddressLength_ = remoteAddressLength;}
+        [[nodiscard]] sockaddr_storage& getRemoteAddress() {return remoteAddress_;}
+
     private:
-
         int sockfd_{-1};
-
         int family_;
+        sockaddr_storage remoteAddress_;
+        socklen_t remoteAddressLength_;
     };
 } // Channel
 
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -5,13 +5,25 @@
 #include <stdexcept>
 #include "../include/Channel.h"
 
+#include "../include/Packet.h"
+
 namespace Channel {
-    Channel::Channel(const int family, const int protocol)
-    : family_(family) {
-        sockfd_ = socket(family, SOCK_RAW, protocol);
+    Channel::Channel(const int family)
+    : family_(family), remoteAddress_({}) {
+        if (family_ == AF_INET) {
+            sockfd_ = socket(family_, SOCK_RAW, IPPROTO_ICMP);
+        } else {
+            sockfd_ = socket(family_, SOCK_DGRAM, IPPROTO_ICMPV6);
+        }
         if (sockfd_ < 0) {
             throw std::runtime_error("Failed to create a socket");
         }
+
+        timeval tv{};
+
+        tv.tv_sec = 2;
+
+        setsockopt(sockfd_, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
     }
 
     Channel::~Channel() {
@@ -20,11 +32,47 @@ namespace Channel {
         }
     }
 
-    ssize_t Channel::sendPacket(const void *data, size_t len, const sockaddr *destination, const socklen_t destinationLength) const {
-        return sendto(sockfd_, data, len, 0, destination, destinationLength);
+    ssize_t Channel::sendPacket(const void *data, const size_t len, const sockaddr *destination) const {
+        return sendto(sockfd_, data, len, 0, destination, remoteAddressLength_);
     }
 
-    ssize_t Channel::receivePacket(void *buffer, size_t len, sockaddr *source, socklen_t *sourceLength) const {
-        return recvfrom(sockfd_, buffer, len, 0, source, sourceLength);
+    ssize_t Channel::receivePacket(void *buffer, const size_t len, sockaddr *source, socklen_t sourceLength) const {
+        return recvfrom(sockfd_, buffer, len, 0, source, &sourceLength);
+    }
+
+    Packet::IcmpPacket Channel::waitForResponse(uint16_t expectedId, sockaddr* source, socklen_t sourceLength) {
+        auto start = std::chrono::steady_clock::now();
+
+        uint8_t buffer[1500];
+
+        while (true) {
+            auto now = std::chrono::steady_clock::now();
+            int timePassed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+
+            if (timePassed >= TIMEOUT_MS) {
+                return {};
+            }
+
+            struct timeval tv{};
+            tv.tv_sec = 0;
+            tv.tv_usec = 200000;
+            setsockopt(sockfd_, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+            ssize_t received = recvfrom(sockfd_, buffer, 1500, 0, source, &sourceLength);
+
+            if (received < 0) {
+                if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                    continue;
+                } else {
+                    return {};
+                }
+            }
+
+            auto packet = Packet::IcmpPacket::parse(buffer, sizeof(buffer));
+
+            if (packet.header.id == expectedId) {
+                return packet;
+            }
+        }
     }
 } // Channel
